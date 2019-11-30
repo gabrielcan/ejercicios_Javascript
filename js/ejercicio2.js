c73922af03302cee5c3667f16c703ee955dfcafd
Gabriel Cano <gabrielcano.86@gmail.com>
2019-11-30 14:10:25 -0300

    se corrige ejercicio2

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,35 +1,46 @@
 
 // ejercicio2
 
-var cantDados= prompt("ingrese cantidad de dados a calcular: ");
-cantDados=parseInt(cantDados);
+var cantDados;
+// cantDados=parseInt(cantDados);
 
 function dado(cantDados) {
 var cont=0
 
-for(var index = 0; index < cantDados; index++) {
+var node1 = document.createElement("li");      
+var node2 = document.createElement("li");              // Create a <li> node
+var textnode1;       // Create a text node
+var textnode2;       // Create a text node
 
+
+
+for(var index = 0; index < cantDados; index++) {
+    
     
         for( var index2=0;index2<6;index2++){
 
+           
 
             for( var index3=0;index3<6;index3++){
+                console.log(index2+1,index3+1);
+                node1 = document.createElement("li");   
+                node2 = document.createElement("li");   
 
-               console.log(index2+1,index3+1)
                 
-               
-                cont=cont+1;
-            }
+                textnode1=document.createTextNode(index2+1);  
+                node1.appendChild(textnode1);  
+                document.getElementById("myList1").appendChild(node1)
+                textnode2=document.createTextNode(index3+1);  
+                node2.appendChild(textnode2);  
+                document.getElementById("myList2").appendChild(node2)
+                 }
 
-
-
-                                            }
 }
 
 }
+}
 
-
-dado(cantDados);
+dado(2);
 
 
 function redireccion() {
